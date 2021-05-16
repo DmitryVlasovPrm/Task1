82b8d5eff5dd945d43a9704610326946981c7f06
Dmitry Vlasov <dima.vlasov@icloud.com>
2021-05-17 00:06:42 +0500

    Начало unit-тестов

---
+++ UnitTests/UsersController_Tests.cs
@@ -0,0 +1,36 @@
+using WebApi;
+using WebApi.Controllers;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UnitTests
+{
+	public class UsersController_Tests
+	{
+		private const string connString = "Server=(localdb)\\MSSQLLocalDB;Database=Server;Trusted_Connection=True;";
+
+		[Fact]
+		public async void GetNonExistentUser()
+		{
+			// Arrange
+			var dbContext = new ServerContext(connString);
+			var controller = new UsersController(dbContext);
+			// Act
+			var result = await controller.Get(9999);
+			// Assert
+			Assert.IsType<NotFoundResult>(result.Result);
+		}
+
+		[Fact]
+		public async void GetAllUsers()
+		{
+			// Arrange
+			var dbContext = new ServerContext(connString);
+			var controller = new UsersController(dbContext);
+			// Act
+			var result = await controller.Get(2006);
+			// Assert
+			Assert.IsType<ObjectResult>(result.Result);
+		}
+	}
+}
